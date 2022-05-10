52e2e95786a40ba34183a5a0a1be11df6f6c984b
Ye Lwin <yelwin2949@gmail.com>
2022-05-10 11:22:54 +0630

    origin structure and data

---
+++ src/theme/images.js
@@ -0,0 +1,13 @@
+
+import purpleThemeboy from '../assets/svg/about/purpleTheme-boy.svg'
+import purpleThemegirl from '../assets/svg/about/purpleTheme-girl.svg'
+
+import eduPurple from '../assets/svg/education/eduPurple.svg'
+
+import expPurple from '../assets/svg/experience/expPurple.svg'
+
+import contactsPurple from '../assets/svg/contacts/contactsPurple.svg'
+
+export { 
+    purpleThemeboy, purpleThemegirl,eduPurple,expPurple,contactsPurple
+}
---
+++ src/theme/theme.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+//data from developer-portfolio
+import {  purpleThemeboy, purpleThemegirl,eduPurple,expPurple,contactsPurple,
+} from './images'
+
+export const myTheme = {
+    type: 'light',
+    primary: '#a164bb',
+    primary400: '#a164bb',
+    primary600: '#a164bb',
+    primary80: '#823ae0cc',
+    primary50: '#823ae080',
+    primary30: '#823ae04d',
+    secondary: '#eaeaea',
+    secondary70: '#eaeaeab3',
+    secondary50: '#eaeaea80',
+    tertiary: '#212121',
+    tertiary80: '#212121cc',
+    tertiary70: '#212121b3',
+    tertiary50: '#21212180',
+    aboutimg1: purpleThemeboy,
+    aboutimg2: purpleThemegirl,
+    eduimg: eduPurple,
+    expimg: expPurple,
+    contactsimg: contactsPurple
+}
+
